6d4c0307a7a057861a1e4579eb66e7b496f1e6c5
Ashadu Zaman <168162401+ashadu-workmates@users.noreply.github.com>
2024-10-25 00:57:40 +0530

    add task

--- src/Components/Reward.jsx
+++ src/Components/Reward.jsx
@@ -6,34 +6,28 @@ import "react-toastify/dist/ReactToastify.css";
 import "../Style/Reward.css";
 
 function Reward() {
+  const [tasks, setTasks] = useState([]);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [link, setLink] = useState("");
-  const [type, setType] = useState("");
   const [reward, setReward] = useState("");
 
-  const AddTask = async () => {
-    if (!title || !description || !link || type === "select") {
-      toast.error("Please fill in all required fields");
-      return;
-    }
+  const [showForm, setShowForm] = useState(false);
 
+  const handleSubmit = async () => {
+    
     try {
       const DataResp = await addDoc(collection(db, "tasks"), {
-        title,
-        description,
-        reward,
-        link,
-        type,
+        tasks,
         createdAt: new Date(),
         createdBy: "User",
         status: "Pending Approval",
+        type: "default",
       });
 
       setTitle("");
       setDescription("");
       setLink("");
-      setType("");
       setReward("");
 
       toast.success("Task added successfully!");
@@ -42,67 +36,103 @@ function Reward() {
     }
   };
 
+  const addTask = () => {
+    if(!title || !description || !link || reward) {
+      return toast.error("All filed is required")
+    }
+    setTasks((prev) => [
+      ...prev,
+      {
+        title,
+        description,
+        link,
+        reward,
+      },
+    ]);
+    setTitle("");
+    setDescription("");
+    setLink("");
+    setReward("");
+    setShowForm(false);
+  };
+
+  const removeTask = (index) => {
+    const newTasks = tasks.filter((task, i) => i !== index);
+    setTasks(newTasks);
+  };
+
   return (
     <div className="task_Container">
       <ToastContainer />
       <div className="task_form">
         <h1>Create Task</h1>
-        <div className="task_form_field">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+
+        <div>
+          {tasks?.map((task, i) => (
+            <div key={i} className="task_item">
+              <p>{task.title}</p>
+              <div onClick={() => removeTask(i)} className="delete-icon">
+                <i className="bi bi-trash"></i>
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="task_form_field">
-          <label htmlFor="desc">Description</label>
-          <textarea
-            id="desc"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div className="task_form_field">
-          <label htmlFor="link">Link</label>
-          <input
-            type="text"
-            id="link"
-            placeholder="Link"
-            value={link}
-            onChange={(e) => setLink(e.target.value)}
-          />
-        </div>
-        <div className="task_form_field">
-          <label htmlFor="reward">Reward</label>
-          <input
-            type="text"
-            id="reward"
-            placeholder="$5"
-            value={reward}
-            onChange={(e) => setReward(e.target.value)}
-          />
-        </div>
-        <div className="task_form_field">
-          <label htmlFor="type">Task Category</label>
-          <select
-            id="type"
-            name="task_type"
-            value={type}
-            onChange={(e) => setType(e.target.value)}
-          >
-            <option value="select">Select</option>
-            <option value="OnChain">OnChain</option>
-            <option value="Social">Social</option>
-          </select>
+        <div className="task_buttons">
+          <button onClick={() => setShowForm(true)} className="add-btn">
+            <i class="bi bi-plus-circle"></i> Add
+          </button>
+          <button onClick={handleSubmit} className="add-btn">
+            Submit
+          </button>
         </div>
 
-        <button onClick={AddTask} style={{ marginTop: "10px" }}>
-          Submit
-        </button>
+        {showForm ? (
+          <div style={{paddingTop: 20}}>
+            <div className="task_form_field">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                id="title"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="task_form_field">
+              <label htmlFor="desc">Description</label>
+              <textarea
+                id="desc"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="task_form_field">
+              <label htmlFor="link">Link</label>
+              <input
+                type="text"
+                id="link"
+                placeholder="Link"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+              />
+            </div>
+            <div className="task_form_field">
+              <label htmlFor="reward">Reward</label>
+              <input
+                type="text"
+                id="reward"
+                placeholder="$5"
+                value={reward}
+                onChange={(e) => setReward(e.target.value)}
+              />
+            </div>
+          
+            <button onClick={addTask} style={{ marginTop: "10px" }}>
+              Add
+            </button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
