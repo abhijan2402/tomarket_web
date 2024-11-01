64d481542a0b1dafa67ab5c5c5313b93dde3dd27
DSRCODE <daljeetkalyanwat62@gmail.com>
2024-11-01 22:47:59 +0530

    updated

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import "./Card.css"
+import "./Card.css";
 
-const Card = ({ title,card_data }) => {
+const Card = ({ title, card_data }) => {
   return (
     <>
-    
-    <div className="card-space">
-      <div className="card_space_details">
-        <h5>{card_data?.tasks[0].title}</h5>
-        <p>+ {card_data?.tasks[0]?.reward} BP</p>
+      <div className="card-space">
+        <div className="card_space_details">
+          <h5>{card_data?.title}</h5>
+          <p>+ {card_data?.reward} BP</p>
+        </div>
+        <div className="card_space_btn">
+          <button className="card_space_btn1">Open</button>
+          <p className="card_space_card_count">
+            0/{card_data?.tasks?.length || 0}
+          </p>
+        </div>
       </div>
-      <div className="card_space_btn">
-        <button className="card_space_btn1">Open</button>
-        <p className="card_space_card_count">0/{card_data?.tasks?.length || 0}</p>
-      </div>
-    </div>
     </>
   );
 };
